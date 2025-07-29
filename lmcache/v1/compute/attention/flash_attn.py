b556a88be7841ef0923d67e02d092b302f2774e1
Hyunsun Chung <hchung1652@sogang.ac.kr>
2025-07-29 05:28:58 +0000

    fixes

--- lmcache/v1/compute/attention/flash_attn.py
+++ lmcache/v1/compute/attention/flash_attn.py
@@ -14,7 +14,7 @@
 
 # Third Party
 from vllm.attention import Attention
-from vllm.v1.attention.backends.flash_attn import FlashAttentionImpl
+from vllm.v1.attention.backends.flash_attn import FlashAttentionImpl, FlashAttentionMetadata
 from vllm.vllm_flash_attn import flash_attn_varlen_func, get_scheduler_metadata
 import torch
 
--- lmcache/v1/gpu_connector.py
+++ lmcache/v1/gpu_connector.py
@@ -340,7 +340,7 @@ class VLLMBufferLayerwiseGPUConnector(GPUConnectorInterface):
         shape = self.get_shape(max_tokens)
         self.dtype = kwargs["dtype"]
         self.device = kwargs["device"]
-        self.query = torch.Tensor(device=self.device)
+        self.query = torch.tensor([1,0], device=self.device)
         
         num_elements = shape.numel()
 
