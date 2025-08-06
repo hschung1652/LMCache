3bafcc04f6241537a15e68107b36315dc08987bd
Hyunsun Chung <hchung1652@sogang.ac.kr>
2025-08-06 08:54:49 +0000

    fixes

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -706,22 +706,21 @@ class LMCacheConnectorV1Impl:
         layer_id = list(map(int, temp))[0]
 
         self.lmcache_engine.offload_gpu.query.copy_(query)
-        key_cache, value_cache = self.lmcache_engine.offload_gpu.kvcaches[layer_id].unbind(0)
         
         reshape_and_cache_flash(
             key,
             value,
-            key_cache,
-            value_cache,
+            self.lmcache_engine.offload_gpu.kvcaches[layer_id].unbind(0)[0],
+            self.lmcache_engine.offload_gpu.kvcaches[layer_id].unbind(0)[1],
             attn_metadata.slot_mapping,
             cache_dtype,
             k_scale,
             v_scale,
         )
 
-        print(f"query: {self.lmcache_engine.offload_gpu.query}")
-        #print(f"key: {key_cache}")
-        #print(f"value: {value_cache}")
+        #print(f"query: {self.lmcache_engine.offload_gpu.query}")
+        print(f"key: {self.lmcache_engine.offload_gpu.kvcaches[layer_id].unbind(0)[0]}")
+        print(f"value: {self.lmcache_engine.offload_gpu.kvcaches[layer_id].unbind(0)[1]}")
 
         output = self.offload_attn.forward_contiguous(self.lmcache_engine.offload_gpu.query, key, value, output, q_scale, k_scale, v_scale)
 
