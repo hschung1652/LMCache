f646a38045d19fcb860dce758ed94b284abf2cf9
Hyunsun Chung <hchung1652@sogang.ac.kr>
2025-08-06 08:27:32 +0000

    fixes

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -719,9 +719,9 @@ class LMCacheConnectorV1Impl:
             v_scale,
         )
 
-        print(f"query: {query}")
-        print(f"key: {key}")
-        print(f"value: {value}")
+        print(f"query: {self.lmcache_engine.offload_gpu.query}")
+        print(f"key: {key_cache}")
+        print(f"value: {value_cache}")
 
         output = self.offload_attn.forward_contiguous(self.lmcache_engine.offload_gpu.query, key, value, output, q_scale, k_scale, v_scale)
 
