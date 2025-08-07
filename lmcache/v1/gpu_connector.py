6c7bfb92d1f4903107a0e50430f3170d16cb2b66
Hyunsun Chung <hchung1652@sogang.ac.kr>
2025-08-07 05:26:11 +0000

    fixes

--- lmcache/v1/gpu_connector.py
+++ lmcache/v1/gpu_connector.py
@@ -679,7 +679,9 @@ class VLLMPagedMemLayerwiseGPUConnector(GPUConnectorInterface):
         print(hidden_dim_size)
         self.query = torch.empty((1,32,128), device=self.device)
 
-        self.kvcaches: Optional[List[torch.tensor]] = List[torch.tensor(device=torch.device(f'cuda:1'))]
+        torch.empty()
+
+        self.kvcaches: Optional[List[torch.Tensor]] = List[torch.empty([1,0],device=torch.device(f'cuda:1'))]
 
         # All sizes are in bytes
         self.element_size = torch.tensor([], dtype=self.dtype).element_size()
